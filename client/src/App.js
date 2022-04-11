a367501b8ab60a59b0be7eba0457408e767a88a9
nf-projects <nf-projects@users.noreply.github.com>
2022-04-10 18:27:41 -0700

    fetch json data from server, then show as text

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react';
+// useState gets a state variable containing the data from the backend
+// ^ will be used to render data on the page
+
+// useEffect is used to fetch the data from the backend on first render
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // fetch the data from "localhost:5000/members"
+    // then get the json from the response
+    // then set the data variable above to the contents of the json
+    fetch("/members").then(
+      response => response.json()
+    ).then(
+      data => {
+        setData(data);
+        console.log(data);
+      }
+    )
+  }, []);
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      {(typeof data.members === 'undefined') ? (
+        <p>Loading Data...</p>
+      ) : (
+        data.members.map((member, i) => (
+          <p key={i}>{member}</p>
+        ))
+      )}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
